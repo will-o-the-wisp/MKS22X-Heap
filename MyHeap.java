c72bba13f87e117c1e629e1ec4ba9333c2fd7496
will-o-the-wisp <kli18@stuy.edu>
2019-04-29 10:56:53 -0400

    empty methods

---
+++ MyHeap.java
@@ -0,0 +1,14 @@
+public class MyHeap{
+  private static void pushDown(int[] data,int size, int index){
+    
+  }
+  private static void pushUp(int[] data, int index){
+
+  }
+  public static void heapify(int[]){
+
+  }
+  public static void heapsort(int[]){
+
+  }
+}
